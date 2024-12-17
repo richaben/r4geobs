6e7bf27db98eef4fbf2ed05680613e25d085d5e6
richaben <brichard.rb@gmail.com>
2024-12-17 16:05:42 +0100

    function modified

--- R/get_geobs_data_flux.R
+++ R/get_geobs_data_flux.R
@@ -1,4 +1,4 @@
-#' Récupérer les données de la base GEOBS
+#' Récupérer les données de la base GEOBS à partir du flux BRGM
 #'
 #' @param couche nom de la couche à charger
 #' @param geo objet spatial de type 'sf'
@@ -6,20 +6,20 @@
 #' @return un data.frame au format 'sf'
 #' @export
 #'
-#' @importFrom httr2 url_parse url_build
+#' @importFrom httr parse_url build_url
 #' @importFrom sf st_bbox read_sf
 #'
 #' @examples
 #' \dontrun{
-#' get_geobs_data("ROE_MONDE", shape_area)
+#' get_geobs_data_flux("ROE_MONDE", shape_area)
 #' }
 
-get_geobs_data <- function(couche, geo){
+get_geobs_data_flux <- function(couche, geo){
 
   geo_bbox <- sf::st_bbox(geo)
 
   url_geobs <- "http://mapsref.brgm.fr/wxs/onema/geobs_monde"
-  url <- httr2::url_parse(url_geobs)
+  url <- httr::parse_url(url_geobs)
 
   url$query <- list(service = "wfs",
                     version = "1.0.0", # optional
@@ -31,15 +31,15 @@ get_geobs_data <- function(couche, geo){
                     #"-1.994019,49.181703,-0.865173,49.777717"
   )
 
-  httr2::url_build(url) %>%
+  httr::build_url(url) %>%
     sf::read_sf(crs = 4326)
 
 }
 
-# get_geobs_data("ROE_MONDE", shape_area) %>% View()
-# get_geobs_data("REFERENTIEL_ROE_MONDE", shape_area) %>% View()
-# get_geobs_data("OBSTACLE_BDOE", shape_area) %>% View()
-# get_geobs_data("OBSTACLE_BDOE_FILTERED", shape_area) %>% View()
-# get_geobs_data("OBSTACLE_ICE", shape_area) %>% glimpse()
+# get_geobs_data_flux("ROE_MONDE", shape_area) %>% View()
+# get_geobs_data_flux("REFERENTIEL_ROE_MONDE", shape_area) %>% View()
+# get_geobs_data_flux("OBSTACLE_BDOE", shape_area) %>% View()
+# get_geobs_data_flux("OBSTACLE_BDOE_FILTERED", shape_area) %>% View()
+# get_geobs_data_flux("OBSTACLE_ICE", shape_area) %>% glimpse()
 
 
